bb7244b430df492a1324896c6deb244cdc1dcae2
akmaljon <akmal@gmail.com>
2024-12-05 17:02:01 +0500

    Layout & Handlebars

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 import express from 'express';
+import { create, engine } from 'express-handlebars';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import hbsHelper from './utils/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
+const hbs = create({
+	defaultLayout: 'main',
+	extname: 'hbs',
+	helpers: hbsHelper,
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-	res.status(200);
-	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+	res.render('index.hbs');
 });
 
 app.get('/about', (req, res) => {
-	res.status(200);
-	res.sendFile(path.join(__dirname, 'views', 'about.html'));
+	res.render('about.hbs');
 });
-
-const __fileName = fileURLToPath(import.meta.url);
 
 const PORT = process.env.PORT || 4100;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
