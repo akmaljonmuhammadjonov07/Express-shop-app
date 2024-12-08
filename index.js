fb715675b49e3e8231cc0f5e019c583600a1b15c
akmaljon <akmal@gmail.com>
2024-12-08 10:33:42 +0500

    MongoDB & .Env

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 import express from 'express';
-import { create, engine } from 'express-handlebars';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import hbsHelper from './utils/index.js';
+import { create } from 'express-handlebars';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+// ROUTES
 import AuthRoutes from './routes/auth.js';
 import ProductsRoutes from './routes/products.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+dotenv.config();
 
 const app = express();
 
-const hbs = create({
-	defaultLayout: 'main',
-	extname: 'hbs',
-	helpers: hbsHelper,
-});
+const hbs = create({ defaultLayout: 'main', extname: 'hbs' });
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
 
-const PORT = process.env.PORT || 4100;
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+const startApp = async () => {
+	try {
+		mongoose.set('strictQuery', false);
+		await mongoose.connect(
+			process.env.MONGO_URI,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			},
+			console.log('Mongo DB connected')
+		);
+
+		const PORT = process.env.PORT || 4100;
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startApp();
