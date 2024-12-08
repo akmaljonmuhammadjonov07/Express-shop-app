e9a6636ecafa29508ea099a1dd942020288d8c5e
akmaljon <akmal@gmail.com>
2024-12-08 13:08:02 +0500

    Validation

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { create } from 'express-handlebars';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-
+import flash from 'connect-flash';
+import session from 'express-session';
 // ROUTES
 import AuthRoutes from './routes/auth.js';
 import ProductsRoutes from './routes/products.js';
@@ -20,6 +21,8 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({ secret: 'Sammi', resave: false, saveUninitialized: false }));
+app.use(flash());
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
